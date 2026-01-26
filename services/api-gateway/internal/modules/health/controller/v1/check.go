e397dea7e158b19f5f72716ec09be2061b2c0cc6
Boklap <121416674+Boklap@users.noreply.github.com>
2026-01-26 11:10:15 +0700

    feat (middleware): merge and update middlewares for api gateway (#39)
    * Feat (Lint): Commit Message Lint
    
    Add commit messag lint in the lint.yml
    
    * refactor (lint): Refactor lint.yml
    
    Update commit-message work flow
    
    * refactor (lint): Refactor lint.yml workflow
    
    Update from check the commit message to check the PR title
    
    * refactor (lint): Refactor lint.yml
    
    Update workflow triggerer
    
    * refactor (lint): Refactor lint.yml
    
    Update the validation rules of pull request title
    
    * feat (identity): Add identity middleware
    
    Add identity middleware and global middleware with project refactoring
    
    * feat (deployment): Add redis
    
    Add redis in docker-compose.yml for development.
    refactor naming convention following linter rules.
    
    ---------
    
    Co-authored-by: Marvino Fransisco <marvino@Marvinos-MacBook-Air.local>

---
+++ services/api-gateway/internal/modules/health/controller/v1/check.go
@@ -0,0 +1,20 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+	"moufube.com/m/internal/appctx/response"
+	"moufube.com/m/internal/modules/health/constant"
+)
+
+// Check godoc
+//
+//	@Summary		Instance Health Check
+//	@Description	Check if the instance is healthy
+//	@Tags			Health
+//	@Accept			json
+//	@Produce		json
+//	@Success		200	{object}	response.SuccessResponse
+//	@Router			/health [get]
+func (hc *HealthController) Check(c *gin.Context) {
+	response.Success(c, constant.InstanceHealthy, nil)
+}
